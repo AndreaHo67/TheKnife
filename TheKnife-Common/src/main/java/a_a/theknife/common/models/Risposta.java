3768c3b56d9b5df570fa2c3268e4c3337a363f5a
Andrea Olmo <aolmo1@studenti.uninsubria.it>
2026-06-21 17:11:16 +0200

    Add files via upload

---
+++ TheKnife-Common/src/main/java/a_a/theknife/common/models/Risposta.java
@@ -0,0 +1,98 @@
+package a_a.theknife.common.models;
+
+import java.io.Serializable;
+
+import java.time.LocalDate;
+
+import a_a.theknife.common.exceptions.TextTooLongException;
+
+public class Risposta implements Serializable{
+    private static final int LUNGHEZZA_MASSIMA_TESTO = 200;
+    
+    private int id;
+    private String autore;
+    private String testo;
+    private LocalDate data;
+    
+    /**
+     * Costruttore incompleto della classe <code>Risposta</code>; costruisce un
+     * oggetto risposta privo di id.
+     * Questo costruttore viene usato per creare nuove risposte, il cui id viene
+     * assegnato dal database in modo seriale.
+     * @param autore
+     * @param testo 
+     * @throws <code>TextTooLongException</code>
+     */
+    public Risposta(String autore, String testo) throws TextTooLongException{
+        if(testo.length() > LUNGHEZZA_MASSIMA_TESTO){
+            throw(new TextTooLongException());
+        }
+        this.autore = autore;
+        this.testo = testo;
+        this.data = LocalDate.now();
+    }
+    
+    /**
+     * Costruttore completo della classe <code>Risposta</code>; costruisce un
+     * oggetto risposta inizializzando tutti i suoi campi, id incluso.
+     * Questo costruttore viene usato per creare oggetti risposta leggendoli
+     * dal database.
+     * @param id
+     * @param autore
+     * @param testo 
+     * @param data 
+     * @throws <code>TextTooLongException</code>
+     */
+    public Risposta(int id, String autore, String testo, LocalDate data) throws TextTooLongException{
+        if(testo.length() > LUNGHEZZA_MASSIMA_TESTO){
+            throw(new TextTooLongException());
+        }
+        this.id = id;
+        this.autore = autore;
+        this.testo = testo;
+        this.data = data;
+    }
+    
+    public int getId(){
+        return(this.id);
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public String getAutore(){
+        return(this.autore);
+    }
+    public void setAutore(String autore){
+        this.autore = autore;
+    }
+    
+    public String getTesto(){
+        return(this.testo);
+    }
+    public void setTesto(String testo) throws TextTooLongException{
+        if(testo.length() > LUNGHEZZA_MASSIMA_TESTO){
+            throw(new TextTooLongException());
+        }
+        this.testo = testo;
+    }
+    
+    public LocalDate getData(){
+        return(this.data);
+    }
+    public void setData(LocalDate data){
+        this.data = data;
+    }
+    
+    /**
+     * Il metodo restituisce una sintetica ed esaustiva descrizione dell'oggetto
+     * risposta.
+     * @return Una stringa contenente le informazioni relative all'oggetto risposta.
+     */
+    @Override
+    public String toString(){
+        return("Risposta n°" + this.id + "\n" +
+               "Lasciata il " + this.data + "\n" +
+               this.testo);
+    }
+}
