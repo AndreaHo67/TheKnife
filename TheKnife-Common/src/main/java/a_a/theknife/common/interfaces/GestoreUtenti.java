f3dcb4f0b9d76f792ea7f54e2302ffa1eeb76039
Andrea Napolitano <205607914+andrenapo@users.noreply.github.com>
2026-06-23 19:57:21 +0200

    Refactor dell'interfaccia GestoreUtenti.java
    Aggiunta dei metodi per tenere traccia dei ristoranti preferiti di un utente

--- TheKnife-Common/src/main/java/a_a/theknife/common/interfaces/GestoreUtenti.java
+++ TheKnife-Common/src/main/java/a_a/theknife/common/interfaces/GestoreUtenti.java
@@ -2,12 +2,10 @@ package a_a.theknife.common.interfaces;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-
 import java.util.ArrayList;
 
 import a_a.theknife.common.models.Utente;
 import a_a.theknife.common.models.Ristorante;
-
 import a_a.theknife.common.exceptions.InvalidUsernameException;
 import a_a.theknife.common.exceptions.InvalidPasswordException;
 
@@ -15,7 +13,7 @@ public interface GestoreUtenti extends Remote{
     /**
      * Il metodo modifica il campo username dell'utente ricevuto come parametro,
      * garantendone l'unicità. Qualora il nuovo username fosse già presente viene
-     * sollevata la <code>InvalidUsernameException</code>
+     * sollevata la <code>InvalidzUsernameException</code>
      * @param utente l'utente a cui modificare lo username
      * @param username il nuovo username
      * @throws RemoteException
@@ -35,5 +33,27 @@ public interface GestoreUtenti extends Remote{
      */
     public void modificaPassword(Utente utente, String password) throws RemoteException, InvalidPasswordException;
     
+    /**
+     * Il metodo restituisce la lista dei ristoranti salvati tra i preferiti dall'utente.
+     * @param utente l'utente di cui si vogliono visualizzare i preferiti
+     * @return ArrayList contenente i ristoranti preferiti
+     * @throws RemoteException 
+     */
+    public ArrayList<Ristorante> visualizzaPreferiti(Utente utente) throws RemoteException;
     
+    /**
+     * Il metodo aggiunge un ristorante alla lista dei preferiti dell'utente.
+     * @param utente l'utente che effettua l'aggiunta ai suoi preferiti
+     * @param ristorante il ristorante da aggiungere ai preferiti
+     * @throws RemoteException 
+     */
+    public void aggiungiPreferito(Utente utente, Ristorante ristorante) throws RemoteException;
+    
+    /**
+     * Il metodo rimuove un ristorante dalla lista dei preferiti dell'utente.
+     * @param utente l'utente che effettua la rimozione di un ristorante dai suoi prefefriti
+     * @param ristorante il ristorante da rimuovere
+     * @throws RemoteException 
+     */
+    public void rimuoviPreferito(Utente utente, Ristorante ristorante) throws RemoteException;
 }
