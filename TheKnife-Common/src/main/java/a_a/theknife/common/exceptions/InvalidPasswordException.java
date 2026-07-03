cf7e8902089c7921ea2ac5538acb50293452eb24
Andrea Olmo <aolmo1@studenti.uninsubria.it>
2026-07-03 22:20:06 +0200

    Delete TheKnife-Common directory

--- TheKnife-Common/src/main/java/a_a/theknife/common/exceptions/InvalidPasswordException.java
+++
@@ -1,6 +0,0 @@
-package a_a.theknife.common.exceptions;
-public class InvalidPasswordException extends Exception{
-    public InvalidPasswordException(){
-        super();
-    }
-}
--- TheKnife-Common/src/main/java/a_a/theknife/common/exceptions/NoReviewsException.java
+++
@@ -1,6 +0,0 @@
-package a_a.theknife.common.exceptions;
-public class NoReviewsException extends Exception{
-    public NoReviewsException(){
-        super();
-    }
-}
